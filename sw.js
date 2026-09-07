7c9fc340571e548c4207cdcd9466c3d829315c16
DonNikolás <nik@nik-jason.ch>
2026-09-07 15:24:49 +0200

    upload old files

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
-const CACHE='mc-v2-cache-1';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));
+const CACHE='marathon-cali-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./sw.js'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
